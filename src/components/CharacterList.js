0784eae7b5eb11d236793a8c0e3c3398cf566b35
Funmi7 <talabifunmilayo@gmail.com>
2019-09-20 08:21:18 +0100

    started project and Installed dependencies

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
