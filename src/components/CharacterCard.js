9b33224eab6323f60c3ff62f49dcade6528bfc68
Funmi7 <talabifunmilayo@gmail.com>
2019-09-20 15:10:35 +0100

    Added some more stylings

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -2,23 +2,41 @@ import React from "react";
 import styled from 'styled-components';
 
 const CharacterCardStyle = styled.div`
-margin-top: 20px;
   display: flex;
   flex-wrap: wrap;
-  flex-direction: row;
+  width: 50%;
+  margin: 20px auto;
+  background-color: black;
+`;
 
+const TextContentStyle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+
+  .textContent {
+    margin-left: 20px;
+
+    h3, p {
+      color: white;
+    }
+  }
 `
 
 export default function CharacterCard(props) {
   // const {name, species, gender, image} = props
   return (
 <CharacterCardStyle>
-  {/* <div> */}
-  <img src={props.character.image} />
-  <h5>{props.character.name}</h5>
-  <p>Specie: {props.character.species}</p>
-  <p>Gender: {props.character.gender}</p>
-  {/* </div> */}
+  <TextContentStyle>
+    <div className="main-img">
+      <img src={props.character.image} />
+    </div>
+    <div className="textContent">
+      <h3>{props.character.name}</h3>
+      <p>Specie: {props.character.species}</p>
+      <p>Gender: {props.character.gender}</p>
+    </div>
+  </TextContentStyle>
 </CharacterCardStyle>
   )
   
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,30 +1,12 @@
-import React, { useState, useEffect } from "react";
-// import Axios from "axios";
-
-
+import React from "react";
 
 export default function SearchForm(props) {
 
 const  {onSearch, searchTerm} = props;
 
-// const [searchValue, setSearchValue] = useState('');
-
-// const handleSearchInputChanges = (e) => {
-//   setSearchValue(e.target.value);
-// }
-
-// const resetInputField = () => {
-//   setSearchValue('')
-// }
-
-// const callSearchFunction = (e) => {
-//   e.preventDefault();
-//   props.search(searchValue);
-//   resetInputField();
-// }
   return (
     <section className="search-form">
-      <label htmlFor='nameInput'>Name</label>
+      <label htmlFor='nameInput'>Enter a Name to search: </label>
         <input onChange={onSearch} value={searchTerm} input id='searchInput' type='text'/>
     </section>
   );
